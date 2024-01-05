89efcf9c0b3c824c2314360a1b016ca949f1ac4b
GabsLopes055 <lopesgabriel055@gmail.com>
2024-01-05 12:07:13 -0300

    Initial commit

---
+++ src/main/java/com/belleBiju/BelleBijuApplication.java
@@ -0,0 +1,13 @@
+package com.belleBiju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelleBijuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelleBijuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belleBiju/BelleBijuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belleBiju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelleBijuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
