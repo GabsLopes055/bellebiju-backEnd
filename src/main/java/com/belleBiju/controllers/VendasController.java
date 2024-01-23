7a27d31d6cc3a2136e1efcdb7c2eefb49d42f195
GabsLopes055 <lopesgabriel055@gmail.com>
2024-01-22 21:51:24 -0300

    Criando os endpoints para deletar e editar uma venda

--- src/main/java/com/belleBiju/controllers/VendasController.java
+++ src/main/java/com/belleBiju/controllers/VendasController.java
@@ -3,6 +3,7 @@ package com.belleBiju.controllers;
 import com.belleBiju.DTOs.requests.VendasRequest;
 import com.belleBiju.DTOs.responses.VendasResponse;
 import com.belleBiju.service.VendasService;
+import jakarta.validation.Path;
 import jakarta.validation.Valid;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "api/vendas")
@@ -38,6 +40,17 @@ public class VendasController {
     @PostMapping("/{inicio}/{fim}")
     public ResponseEntity<List<VendasResponse>> listAllVendasByDates(@PathVariable(value = "inicio") String inicio, @PathVariable(value = "fim") String fim) {
         return ResponseEntity.status(HttpStatus.OK).body(service.listVendasByDates(inicio,fim));
+    }
+
+    @PutMapping("/{idVenda}")
+    public ResponseEntity<VendasResponse> editVenda(@PathVariable(value = "idVenda") UUID idVenda, @RequestBody @Valid VendasRequest request) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.service.editVenda(request, idVenda));
+    }
+
+    @DeleteMapping("{idVenda}")
+    public ResponseEntity<String> deleteVenda(@PathVariable(value = "idVenda") UUID idVenda) {
+        if(this.service.deleteVenda(idVenda)) return ResponseEntity.status(HttpStatus.OK).body("Venda Deletada");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao Excluir Venda !");
     }
 
 
--- src/main/java/com/belleBiju/service/VendasService.java
+++ src/main/java/com/belleBiju/service/VendasService.java
@@ -4,10 +4,13 @@ import com.belleBiju.DTOs.requests.VendasRequest;
 import com.belleBiju.DTOs.responses.VendasResponse;
 import com.belleBiju.entities.Vendas;
 import com.belleBiju.repository.VendasRepository;
+import com.belleBiju.service.exceptions.EntityNotFound;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class VendasService {
@@ -19,7 +22,7 @@ public class VendasService {
     }
 
     /*Metodo para salvar uma venda*/
-    public VendasResponse createVenda(VendasRequest request){
+    public VendasResponse createVenda(VendasRequest request) {
 
         VendasRequest response = new VendasRequest();
 
@@ -61,6 +64,34 @@ public class VendasService {
         return responseVendas;
     }
 
+    public VendasResponse editVenda(VendasRequest request, UUID idVenda) {
+
+        Optional<Vendas> findVenda = Optional.ofNullable(repository.findById(idVenda).orElseThrow(() -> new EntityNotFound("Venda não encontrada !")));
+
+        Vendas venda = findVenda.get();
+
+        venda.setNomeProduto(request.getNomeProduto());
+        venda.setPreco(request.getPreco());
+        venda.setTotal(request.getTotal());
+        venda.setQuantidade(request.getQuantidade());
+        venda.setFormaPagamento(request.getFormaPagamento());
+
+        this.repository.save(venda);
+
+        return new VendasResponse().toModelResponse(venda);
+
+    }
+
+    public boolean deleteVenda(UUID idVenda) {
+
+        Optional<Vendas> findVenda = Optional.ofNullable(repository.findById(idVenda).orElseThrow(() -> new EntityNotFound("Venda não encontrada !")));
+
+        if (findVenda.isEmpty()) return false;
+
+        this.repository.delete(findVenda.get());
+        return true;
+
+    }
 
 
 }
