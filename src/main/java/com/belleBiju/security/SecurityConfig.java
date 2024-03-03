48c3079c6c1888bb2f93f6fd261a2c6f0fa39d2e
GabsLopes055 <lopesgabriel055@gmail.com>
2024-03-03 20:10:00 -0300

    application.properties

--- src/main/java/com/belleBiju/security/SecurityConfig.java
+++ src/main/java/com/belleBiju/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.belleBiju.security;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -14,9 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
@@ -31,16 +27,6 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        // Configuração do filtro CORS
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.applyPermitDefaultValues(); // Permite todos os métodos, cabeçalhos e origens
-        corsConfig.addAllowedOrigin("https://bellebiju.vercel.app"); // Permite apenas a origem específica
-        corsConfig.setAllowCredentials(true); // Permite credenciais (cookies)
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        CorsFilter corsFilter = new CorsFilter();
 
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -49,7 +35,6 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST,"/api/authentication/login").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class) // Adiciona o filtro CORS aqui
                 .build();
     }
 
